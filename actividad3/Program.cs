604416727ff0fdd3fb6b14d7a730fe3053cd827d
Alkhemd <alankuk6@gmail.com>
2025-04-07 16:41:34 -0600

    Agregando actividad3

---
+++ actividad3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ConsumoAgua
+{
+    public double metrosCubicos { get; set; }
+    public double litros { get; set; }
+
+    public ConsumoAgua(double metrosCubicos)
+    {
+        this.metrosCubicos = metrosCubicos;
+        this.litros = metrosCubicos * 1000;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el consumo en metros cúbicos (m³):");
+        double m3 = Convert.ToDouble(Console.ReadLine());
+
+        ConsumoAgua consumo = new ConsumoAgua(m3);
+
+        Console.WriteLine("El consumo en litros es: " + consumo.litros + " litros");
+    }
+}
